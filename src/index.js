b51649f69f2a5bbecd4a9c664738eeec4450592c
Georg Skuggedal <georgskuggedal@icloud.com>
2020-06-18 14:50:29 +0200

    Trying again...

--- src/index.js
+++ src/index.js
@@ -181,8 +181,11 @@ autoUpdater.on("update-not-available", () => {
   sendStatusToWindows("Update not available")
 })
 
-autoUpdater.on("download-progress", (progress) => {
-  sendStatusToWindows("Progress: "  + progress.percent)
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
 })
 
 autoUpdater.on("update-downloaded", (info) => {
