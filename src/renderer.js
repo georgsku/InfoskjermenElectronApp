b37fe43775b93b3325f89475466d46b5c3ddbc87
Georg Skuggedal <georgskuggedal@icloud.com>
2020-06-18 15:04:02 +0200

    new version

--- src/renderer.js
+++ src/renderer.js
@@ -1,8 +1,8 @@
 const { ipcRenderer } = require('electron');
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 console.log("Connected")
-console.log("1.0.0!")
-console.log("base version!!")
+console.log("1.0.1!")
+console.log("new version!!")
 
 let host = "http://app.infoskjermen.no"
 //let host = "http://10.0.1.10:3000/v10/ppm128"
