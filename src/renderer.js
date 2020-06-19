d7202716b546e9cf23ce68cbedb495f11c27b4b3
Georg Skuggedal <georgskuggedal@icloud.com>
2020-06-19 14:14:08 +0200

    App is now working for linux and win

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,6 @@
 const { ipcRenderer } = require('electron');
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 console.log("Connected")
-console.log("1.0.6!")
 
 let host = "http://app.infoskjermen.no"
 //let host = "http://10.0.1.10:3000/v10/ppm128"
@@ -16,6 +15,7 @@ window.onload = function() {
     storagehost = myStorage.getItem("host") 
     
     console.log(myStorage.getItem("host"))
+    console.log(window.location.hash.substring(1))
 
     iframe_load_go()
     
